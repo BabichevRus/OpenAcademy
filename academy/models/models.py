cf31b984e3da5cce7a4214273968801add7172d8
BabichevRus <rbabichwev>
2022-04-29 18:54:01 +0300

    learn Odoo (Open Academy)

--- academy/models/models.py
+++ academy/models/models.py
@@ -11,7 +11,7 @@ class Teachers(models.Model):
 class Courses(models.Model):
     _name = 'academy.courses'
     #_inherit = ['mail.thread', 'product.template']
-    _inherit = ['mail.thread']
+    _inherit = ['product.template']
 
     name = fields.Char()
     teacher_id = fields.Many2one('academy.teachers', string="Teacher")
