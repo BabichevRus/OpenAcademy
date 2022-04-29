ad920468d5bf678808d121c98dbee1f921d4b0ce
BabichevRus <rbabichwev>
2022-04-29 17:21:06 +0300

    learn Odoo (Open Academy)

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['mail', 'website'],
+    'depends': ['mail', 'website', 'website_sale'],
 
     # always loaded
     'data': [
